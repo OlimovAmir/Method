09672c16cc6262485f35f9a6a40f947260c86d49
olimov Amir <olimov.amir@mail.ru>
2023-11-23 12:25:55 +0300

    Добавьте файлы проекта.

---
+++ MethodExample.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+     class MethodExample
+    {
+        static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Sum(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        static void Main(string[] args)
+        {
+            int result = Sum(1, 2);
+            Console.WriteLine(result);
+        }
+    }
+}
